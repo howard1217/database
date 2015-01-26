bf4564aa3d482ddd83d5868f0552f3d483448fc0
vagrant <vagrant@precise64.(none)>
2015-01-26 06:53:40 +0000

    possibly passing ebook.csv

--- hw1/hw1.sh
+++ hw1/hw1.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # bash command-line arguments are accessible as $0 (the bash script), $1, etc.
 # echo "Running" $0 "on" $1
-echo "Replace the contents of this file with your solution."
+# vagrant@precise64:~$ ./hw1.sh ebooks_tiny.txt
+
+echo "Running hw1."
+echo "title,author,release_date,ebook_id,language,body" > ebook.csv
+echo "token,count" > token_counts.csv
+
+# assuming "ebook_tiny.txt is $1"
+python hw1.py $1
+
+exit 0
